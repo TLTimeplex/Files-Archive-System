013dcbc664b5eb692071d02376c55deccbcbdfe8
Timeplex <72736935+TLTimeplex@users.noreply.github.com>
2023-04-07 22:08:15 +0200

    Added Route Blocking Element

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,17 @@ import React, { ReactNode } from 'react';
 import { BrowserRouter as Router, Route, useLocation, Routes } from 'react-router-dom';
 import './App.css';
 import Main from './features/main';
+import { RouteCustoms } from './modules/RouteCustoms';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='*' element={<BlockUrl element={<Main />} />} />
+        <Route path='*' element={<RouteCustoms element={<Main />} blocked={["/login"]} />} />
         <Route path='/login' element={<></>}  />
       </Routes>
     </Router>
   );
-}
-
-interface BlockUrlProps {
-  element: JSX.Element;
-}
-
-const BlockUrl = ({element} : BlockUrlProps) : JSX.Element => {
-  const location = useLocation();
-
-  const bannedUrls = ["/login"];
-
-  if(bannedUrls.includes(location.pathname))
-    return <></>;
-
-  else
-    return element;
 }
 
 export default App;
---
+++ src/modules/RouteCustoms/index.tsx
@@ -0,0 +1,16 @@
+import { useLocation, RouteProps, Route} from 'react-router-dom';
+
+
+export interface RouteCustomsProps {
+  element: JSX.Element;
+  blocked?: string[];
+}
+
+export const RouteCustoms = (props: RouteCustomsProps) : React.ReactElement | null => {
+  const location = useLocation();
+
+  if(props.blocked?.includes(location.pathname))
+    return null;
+    
+  return props.element;
+};
