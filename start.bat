858b4f1dae7f623222d00a9f36062585aef9e2a0
Tim <72736935+TLTimeplex@users.noreply.github.com>
2023-06-13 22:12:50 +0200

    Test 3

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
 start "api"    cmd /c "cd api    && npm i && set PORT=3000 && npm start"
 start "client" cmd /c "cd client && npm i && set PORT=2000 && npm start"
-start "proxy"  cmd /c "cd proxy  && npm i && npm start"
+start "proxy"  cmd /c "cd proxy  && npm i && npm start" 
