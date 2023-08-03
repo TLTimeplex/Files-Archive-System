1e112412cd3239215de9b4f08e32ce7d9ff7be42
Tim <72736935+TLTimeplex@users.noreply.github.com>
2023-08-03 15:49:10 +0200

    Restyled

--- client/src/features/Dashboard/index.tsx
+++ client/src/features/Dashboard/index.tsx
@@ -1,13 +1,22 @@
 import { LastFileChanged } from "./LastChangedFiles/lastChangedFiles";
 import { Piechart } from "./Piechart/piechart"
+import "./style.css"
 
 export const Dashboard = () => {
-    return (
-        <div>
-            <Piechart />
-            <LastFileChanged/> 
-        </div>
-    );
+  return (
+    <div id="DashboardGrid">
+      <div className="DashboardBox">
+        <Piechart />
+      </div>
+      <div className="DashboardBox">
+        <LastFileChanged />
+      </div>
+      <div className="DashboardBox" />
+      <div className="DashboardBox" />
+      <div className="DashboardBox" />
+      <div className="DashboardBox" />
+    </div>
+  );
 }
 
 export default Dashboard;
